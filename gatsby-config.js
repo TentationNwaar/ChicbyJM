0f636a826088b96b7f35f326866daf9ea2322b89
Max <62596612+TentationNwaar@users.noreply.github.com>
2024-08-08 21:38:23 +0200

    Ajout de la traduction auto manuelle

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,29 @@ module.exports = {
     title: `Gatsby Sydney Ecommerce Theme`,
     siteUrl: `https://jamm.matter.design`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-react-i18next',
+      options: {
+        localeJsonSourceName: 'locales', // nom de la source de données
+        languages: ['en', 'fr'], // langues supportées
+        defaultLanguage: 'en', // langue par défaut
+        siteUrl: 'https://chicbyjm.netlify.app/',
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false, // pas nécessaire pour React
+          },
+          keySeparator: false,
+          nsSeparator: false,
+        },
+        pages: [
+          {
+            matchPath: '/:lang?/about',
+            getLanguageFromPath: true,
+          },
+          // plus de configurations de pages si nécessaire
+        ],
+      },
+    },
+  ],
 };
